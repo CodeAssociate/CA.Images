46763cd3fe6f34230d103970fb27519510b3844d
Kevin Bosch <8487763+KevinBosch@users.noreply.github.com>
2022-04-23 15:45:52 +1200

    Add files back in

---
+++ CA.Blocks.Images/DependencyInjection/ImagesDependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using CA.Blocks.Images.Resize;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CA.Blocks.Images.DependencyInjection
+{
+    public static  class ImagesDependencyInjection
+    {
+        public static IServiceCollection AddCaBlocksImages(this IServiceCollection services)
+        {
+            services.AddTransient<IImageResizerLib, ImageResizerLib>();
+
+            return services;
+        }
+    }
+}
