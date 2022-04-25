6069f60280c3afb37cc544a7c6566f6863c70c6e
Kevin Bosch <8487763+KevinBosch@users.noreply.github.com>
2022-04-25 18:48:30 +1200

    Add Orientation fix

--- CA.Blocks.Images/Resize/ImageResizer.cs
+++ CA.Blocks.Images/Resize/ImageResizer.cs
@@ -1,11 +1,30 @@
-﻿using System.Drawing;
+﻿using System.Collections;
+using System.Drawing;
 using System.Drawing.Drawing2D;
 
 namespace CA.Blocks.Images.Resize
 {
+
+
+
     public class ImageResizer
     {
         public static ImageResizer Instance => new ImageResizer();
+
+        private class OrientationValue
+        {
+            public const int OrientationKey = 0x0112;
+            public const int NotSpecified = 0;
+            public const int NormalOrientation = 1;
+            public const int MirrorHorizontal = 2;
+            public const int UpsideDown = 3;
+            public const int MirrorVertical = 4;
+            public const int MirrorHorizontalAndRotateRight = 5;
+            public const int RotateLeft = 6;
+            public const int MirorHorizontalAndRotateLeft = 7;
+            public const int RotateRight = 8;
+        }
+
 
         public Image ResizeImage(Image imgToResize, Size maxSize)
         {
@@ -23,6 +42,43 @@ namespace CA.Blocks.Images.Resize
             var g = Graphics.FromImage(iBitmap);
             g.InterpolationMode = InterpolationMode.HighQualityBicubic; // do the highest quality resize
             g.DrawImage(imgToResize, 0, 0, destWidth, destHeight);
+
+            // Fix orientation if needed.
+            if (((IList)imgToResize.PropertyIdList).Contains(OrientationValue.OrientationKey))
+            {
+                var orientation = (int)imgToResize.GetPropertyItem(OrientationValue.OrientationKey).Value[0];
+                switch (orientation)
+                {
+                    case OrientationValue.NotSpecified: // Assume it is good.
+                    case OrientationValue.NormalOrientation:
+                        // No rotation required.
+                        break;
+                    case OrientationValue.MirrorHorizontal:
+                        iBitmap.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                        break;
+                    case OrientationValue.UpsideDown:
+                        iBitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        break;
+                    case OrientationValue.MirrorVertical:
+                        iBitmap.RotateFlip(RotateFlipType.Rotate180FlipX);
+                        break;
+                    case OrientationValue.MirrorHorizontalAndRotateRight:
+                        iBitmap.RotateFlip(RotateFlipType.Rotate90FlipX);
+                        break;
+                    case OrientationValue.RotateLeft:
+                        iBitmap.RotateFlip(RotateFlipType.Rotate90FlipNone);
+                        break;
+                    case OrientationValue.MirorHorizontalAndRotateLeft:
+                        iBitmap.RotateFlip(RotateFlipType.Rotate270FlipX);
+                        break;
+                    case OrientationValue.RotateRight:
+                        iBitmap.RotateFlip(RotateFlipType.Rotate270FlipNone);
+                        break;
+                    default:
+                        // we dont know do do nothing
+                        break;
+                }
+            }
             g.Dispose();
             return iBitmap;
         }
--- docs/index.md
+++ docs/index.md
@@ -1,5 +1,36 @@
-## CA.Images
+# CA.Images
 ![Build Status](https://github.com/codeassociate/CA.Images/actions/workflows/BuildAndTest.yml/badge.svg)
+
+# An Bulk Image Resizer 
+
+This is an open source project aiming to provide a little utility to enable bulk resizing of images on a local machine.
+The premise is that you have a bunch of images all within the same directory that need to be resized keeping the existing aspect ratio, but making the images smaller. 
+
+## Use cases:
+1. Websites wanting to make their existing images smaller for faster download times:
+2. Websites wanting to provide small preview sized of their existing images 
+3. Some websites will not allow uploading of full size images, the need to be resize first.
+4. Am sure there are many other reasons.
+
+
+#Quick Example 
+The following code will resize all images in the current directory to a max width of 1024 pixels. 
+``` 
+C:\MyImageFOlder>CA.Images resize -w 1024
+```
+
+The original images are not modified, the resize images will places in a nested directory called "ResizedImages" By default the name of the file will be 
+
+{original file name}{width}x{height}.{original file type}
+
+
+
+
+
+
+
+
+
 
 ### More info coming
 This learning about the gt hub process flow Code => actions => pages and packages
